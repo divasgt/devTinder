f33fe521bb6bc520fa332ff0af3a8eccb0c42621
Divas <divas.verma1956@gmail.com>
2026-05-02 15:42:44 +0530

    multiple-route-handlers

--- src/app.js
+++ src/app.js
@@ -2,24 +2,35 @@ const express = require("express");
 
 const app = express();
 
-// this will only handle GET call to /user
-app.get("/user", (req, res) => {
-  res.send({ firstName: "Divas", lastName: "Verma" });
-});
+// these all below perfectly valid syntax, all will work the same way. (passing route handler functions in Array) and We can write as many route handlers as we want. Not just in app.use, but in app.get, app.post, in all.
+// app.use("/route", rH1, rH2, rH3, rH4, rH5)
+// app.use("/route", [rH1, rH2, rH3, rH4, rH5])
+// app.use("/route", [rH1, rH2, rH3], rH4, rH5)
+// app.use("/route", rH1, [rH2, rH3], rH4, rH5)
 
-app.post("/user", (req, res) => {
-  // saving data to DB
-  res.send("Data successfully saved to the database!");
-});
+// if we don't send any response in route handler function, then the request will keep going or hang, and no output.
+// if we already sent a response, and then we sent another response in next route handler, then it wil give error, that response already sent, and so connection is closed, and response again will not be sent.
+// if we wrote next() but there is no next route handler function passed and we haven't sent any response yet, then it will give error in client sede cannot get /route
 
-app.delete("/user", (req, res) => {
-  res.send("Deleted successfully!");
-});
-
-// this will match all the HTTP method API calls to test, like GET, POST
-app.use("/test", (req, res) => {
-  res.send("Hello from the server!");
-});
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("Handling the route user!!");
+    // res.send("Response!!");
+    // when we call this next function, then next route handler function is called.
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling the route user 2!!");
+    // res.send("2nd Response!!");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling the route user 3!!");
+    res.send("3rd Response!!");
+    next();
+  },
+);
 
 app.listen(4000, () => {
   console.log("Server is successfully listening on port 4000...");
